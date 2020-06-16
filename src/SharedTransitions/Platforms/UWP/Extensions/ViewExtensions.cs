bf6baf536e7204a5dba6b9acd44b0e7e94a126ad
Craig Wittenberg <craigwi@seanet.com>
2020-06-15 20:10:30 -0700

    Add capability for UAP sources and empty tests; required adding TargetFrameworks list to shared TransitionApp.csproj.

---
+++ src/SharedTransitions/Platforms/UWP/Extensions/ViewExtensions.cs
@@ -0,0 +1,10 @@
+﻿﻿using System;
+using Xamarin.Forms;
+using Color = Xamarin.Forms.Color;
+
+namespace Plugin.SharedTransitions.Platforms.UWP
+{
+    internal static class ViewExtensions
+    {
+    }
+}
---
+++ src/SharedTransitions/Platforms/UWP/Renderers/SharedTransitionNavigationRenderer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Plugin.SharedTransitions;
+using Plugin.SharedTransitions.Platforms.UWP;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+
+[assembly: ExportRenderer(typeof(SharedTransitionNavigationPage), typeof(SharedTransitionNavigationRenderer))]
+
+namespace Plugin.SharedTransitions.Platforms.UWP
+{
+    public class SharedTransitionNavigationRenderer : NavigationPageRenderer
+    {
+        public SharedTransitionNavigationRenderer()
+        {
+
+        }
+    }
+}
